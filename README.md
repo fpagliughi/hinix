e59c84f5583ea852a0a40d9c703a938d551c90a7
fmp <fpagliughi@mindspring.com>
2018-09-06 14:52:09 -0400

    Initial crate. Implemented EventFd

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hinix
-High level *nix functionality in Rust
+High level *nix functionality in Rust.
+
+This sits atop the awesome Rust [nix](https://github.com/nix-rust/nix) crate and provides higher-orer functionality for systems programming on a *nix (Linux, Posix, Unix, etc) operating system.
+
---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+// hinix/src/lib.rs
+
+//#[macro_use]
+//extern crate log;
+
+extern crate nix;
+extern crate libc;
+
+pub use eventfd::*;
+
+pub mod eventfd;
+
+// Hinix Result type
+pub type Result<T> = nix::Result<T>;
+
+pub type Error = nix::Error;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
